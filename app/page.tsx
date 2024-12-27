d6d7221911c8175650374491f95f2ee1ad4f0658
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-12-27 11:21:16 -0800

    cleaning up

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import About from "./about";
-import Projects from "./projects";
 import Blog from "./blog";
-import Contact from "./contact";
-import Experience from "./experience";
 import FirstPage from "./firstPage";
 
 export default function Home() {
@@ -10,10 +7,7 @@ export default function Home() {
     <main className="min-h-screen">
       <FirstPage />
       <About />
-      <Projects />
-      <Experience />
       <Blog />
-      <Contact />
     </main>
   );
 }
