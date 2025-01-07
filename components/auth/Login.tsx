125342397287b76d895c65f273da900d8f220328
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2025-01-06 17:36:06 -0800

    adding style

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -12,7 +12,6 @@ const Login = ({ onLogin }: { onLogin: () => void }) => {
       await signInWithEmailAndPassword(auth, email, password);
       onLogin(); // Call the onLogin callback if login is successful
     } catch (error: any) {
-      // Check if the error has a code property (Firebase-specific errors do)
       if (error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found') {
         alert('Incorrect email or password. Please try again.');
       } else {
@@ -23,23 +22,45 @@ const Login = ({ onLogin }: { onLogin: () => void }) => {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <div className="w-full max-w-sm p-6 bg-gray-800 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-center text-[#a770ad] mb-6">Editor Login</h2>
+        <form className="space-y-4" onSubmit={handleLogin}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-200">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+              className="w-full px-4 py-2 mt-1 text-gray-800 bg-gray-200 rounded-lg focus:ring-2 focus:ring-orange-500 focus:outline-none"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-200">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+              className="w-full px-4 py-2 mt-1 text-gray-800 bg-gray-200 rounded-lg focus:ring-2 focus:ring-orange-500 focus:outline-none"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 font-bold text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 focus:outline-none"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
