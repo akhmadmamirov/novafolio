5cde99b49bd4bac60e711f8baa74bd0528afd426
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2025-01-08 17:08:14 -0800

    init post details

--- data/posts.ts
+++ data/posts.ts
@@ -2,24 +2,56 @@ import Posts from "@/components/Posts"
 
 export const posts : Posts = [
   {
-    "title": "Understanding Static Bug Detection",
-    "content": "Static bug detection is a critical component of software development...",
+    "title": "SWE Internship at EA",
+    "content": `
+      I had the amazing opportunity to intern at Electronic Arts (EA) during the summer of 2024 in the commerce and Identity team. This experience not only allowed me to develop my technical skills but also helped me grow professionally by working alongside talented engineers in a fast-paced, creative environment. I contributed to building and improving systems that handled real-time transactions and user identity management for EA's gaming ecosystem. The exposure to these large-scale systems was both challenging and rewarding.\n\n
+      One of the highlights of my internship was participating in the Intern Early Access program at EA, where I had the chance to visit the company's headquarters in Redwood Shores, CA. This program provided an excellent opportunity to network with fellow interns and senior executives. I met with EA’s CEO and other leaders, learning about the company's vision and future direction. The experience helped me understand how EA fosters a culture of innovation and collaboration.\n\n
+      Throughout my internship, I worked closely with my teammates and had a great time learning and contributing to the teams goals. I participated in team-building activities, including regular basketball games with my fellow interns, which strengthened our bond and collaboration. I also took part in networking events, learned how to create design documents, and even delivered a final presentation at the intern fair. It was a perfect blend of learning, development, and fun that made my internship at EA unforgettable.\n\n
+    \n`,
     "imageURL": "/ea_group.png",
     "createdAt": "2025-01-01T10:00:00Z",
     "published": true,
-    "slug": "understanding-static-bug-detection",
-    "tags": ["software", "bug-detection", "LLM"],
+    "slug": "ea-swe-internship",
+    "tags": ["internship", "experience", "LLM"],
     "updatedAt": "2025-01-05T14:30:00Z",
     "thumbNail": "/ea_group.png"
   },
   {
-    "title": "Exploring AI Infrastructure Challenges",
-    "content": "Multi-cloud complexity has become a pressing challenge in AI infrastructure...",
-    "imageURL": "/ea_group.png",
+    "title": "Attending Google/IO",
+    "content": `In May 2024, I had the incredible opportunity
+              to attend Google I/O, an event that not only deepened my
+              technical knowledge but also inspired me to push the boundaries
+              of innovation. The sessions at Google I/O were
+              filled with cutting-edge technologies and product announcements
+              that sparked new ideas for my future projects. Being surrounded
+              by brilliant minds in the tech industry, I learned a lot about 
+              the latest advancements in AI, cloud computing, and web technologies.
+              The energy at the conference was electrifying, and I left with a renewed passion
+              to implement the ideas I gained at the event into real-world 
+              applications.\n\nMy journey into the Google Developer Student Clubs
+              (GDSC) community began when I founded the GDSC chapter at the College of Wooster.
+              I saw an opportunity to build a space where students could collaborate,
+              learn, and grow in their technical skills. The initiative was a way for
+              me to give back to the community and help my peers access resources and
+              opportunities from Google. I led the club with the vision of empowering
+              students to engage in projects that solve real-world problems, fostering an
+              environment of innovation and collaboration. As a leader, I organized events
+              such as hackathons, workshops, and coding challenges that gave students
+              the chance to network, learn from experts, and improve their technical
+              skills.\n\nAttending Google I/O in 2024 was a dream come true for me.
+              It was more than just a chance to explore the newest trends in technology—it
+              was a recognition of the work I had done through GDSC and a chance to connect
+              with like-minded individuals. The event reinforced the importance of community
+              in the tech world, something I am passionate about as I continue to grow the GDSC
+              chapter at my college. Being part of the global developer ecosystem, I left
+              Google I/O feeling inspired and determined to take the lessons learned and
+              apply them to my work with the GDSC community back home, ensuring we remain
+              at the forefront of tech innovation and collaboration.`,
+    "imageURL": "/google_io.png",
     "createdAt": "2024-12-15T08:45:00Z",
     "published": false,
-    "slug": "exploring-ai-infrastructure-challenges",
-    "tags": ["AI", "cloud", "infrastructure"],
+    "slug": "google-io-2023",
+    "tags": ["AI", "Cloud", "Conference"],
     "updatedAt": "2024-12-20T11:00:00Z",
     "thumbNail": "/google_io.png"
   }
