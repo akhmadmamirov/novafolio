bba4156cf4748cc469ab590021e5787cbf131b3f
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2025-01-12 21:37:11 -0500

    adding loader

--- components/VisitorCounter.tsx
+++ components/VisitorCounter.tsx
@@ -3,6 +3,7 @@ import { Eye } from 'lucide-react';
 
 const VisitorCounter = () => {
   const [count, setCount] = useState(0);
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchCount = async () => {
@@ -10,7 +11,9 @@ const VisitorCounter = () => {
         const response = await fetch("/api/visitorCount");
         const data = await response.json();
         setCount(data.count);
+        setLoading(false)
       } catch (error) {
+        setLoading(false)
         console.error("Error fetching visitor count:", error);
       }
     };
@@ -21,7 +24,11 @@ const VisitorCounter = () => {
   return (
     <div className="flex items-center text-white gap-2">
       <Eye size={20} />
-      <span>{count}</span>
+        {isLoading ? (
+          <div className="animate-spin w-5 h-5 border-4 border-t-transparent border-blue-500 rounded-full"></div> // Simple spinner as loader
+        ) : (
+          <span>{count}</span>
+        )}
     </div>
   );
 };
